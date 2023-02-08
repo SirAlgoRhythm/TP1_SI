31609516640155be36c323b4419e396275f499bb
Elizabeth0014 <115196592+Elizabeth0014@users.noreply.github.com>
2023-02-08 15:56:18 -0500

    Algoritme de jeton en c#

---
+++ ClientSideApp/ClientSideApp/AlgoritmeJeton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSideApp
+{
+	internal class AlgoritmeJeton
+	{
+		static string GenererJeton()
+		{
+			string key = Math.Round((double)(DateTime.Now.Ticks) / 60 / 1000).ToString();
+			string hash = "";
+			ulong numberHash = 0;
+
+			for (int i = 0; i < 5; i++)
+			{
+				ulong temp = (ulong)key[i];
+				if (i < 1) { numberHash = temp; }
+				else { numberHash = ulong.Parse(numberHash.ToString() + temp.ToString()); }
+			}
+
+			hash = (numberHash * (numberHash % 13)).ToString();
+
+			hash.ToCharArray().Reverse();
+			return hash.Substring(0, 8);
+		}
+	}
+}
